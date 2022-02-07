af56a2eea30cd4b235cd694754a778f0136263d0
KaiTheBrother <99162662+KaiTheBrother@users.noreply.github.com>
2022-02-07 12:55:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Hiiii :D",
+    introTitle: 'xin chào cậu người ở trước màn hình 😏💅',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi =((
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích Vy lắm lun á',
+    btnNo: 'Không thích Vy ,-,',
+    question: 'Tại sao cậu thích tớ 😼',
+    btnReply: 'Gửi cho Vy ',
+    reply: 'Tại tớ thích cậu thôi  ',
+    mess: 'ghê z :0 lần đầu tiên có người thích tớ á',
+    messDesc: 'ăn ốc quế sầu riêng cùng với tớ ko 🙀?.',
+    btnAccept: 'Okiiii',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
