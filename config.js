475620bddaf5f286565c8d8d73379ba02ef98c09
KaiTheBrother <99162662+KaiTheBrother@users.noreply.github.com>
2022-02-07 19:31:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'ỏmg =))',
     messDesc: 'ăn ốc quế sầu riêng cùng với tớ ko 🙀?.',
     btnAccept: 'Okiiii',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.google.com/search?q=%E1%BB%91c+qu%E1%BA%BF+s%E1%BA%A7u+ri%C3%AAng&sxsrf=APq-WBuCWOfqgbDsG2Sd-MXfjn5FB-uqwQ:1644236871965&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiT77fVy-31AhWUVPUHHcbrAEsQ_AUoAXoECAEQAw&biw=1366&bih=657&dpr=1#imgrc=-zgcabiycmgsiM' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
