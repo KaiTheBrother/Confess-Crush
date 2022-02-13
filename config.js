4e79e21a6dd26e0094bbf65da4922efb80d7a5d4
KaiTheBrother <99162662+KaiTheBrother@users.noreply.github.com>
2022-02-13 08:17:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     titleWeb: "Hiiii :D",
     introTitle: 'xin chào cậu người ở trước màn hình 😏💅',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi =((
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introDesc: `Nụ cười của Super Idol
+Cũng không ngọt như cậu
+Cái nắng chói chang giữa tháng 8
+Cũng không tỏa nắng bằng cậu =))`,
+    btnIntro: ':D',
     title: 'Chọn nút thích hay ko thích đi nha tr',
     desc: 'Nhớ chọn nhanh á ',
     btnYes: 'Thích Vy lắm lun á',
@@ -13,7 +13,7 @@ const CONFIG = {
     question: 'Tại sao cậu thích tớ 😼',
     btnReply: 'Gửi cho Vy ',
     reply: 'Tại Vy đẹp try 😼💅🏻',
-    mess: 'ỏmg =))',
+    mess: 'omg  =))',
     messDesc: 'ăn ốc quế sầu riêng cùng với tớ ko 🙀?.',
     btnAccept: 'Okiiii',
     messLink: 'https://www.google.com/search?q=%E1%BB%91c+qu%E1%BA%BF+s%E1%BA%A7u+ri%C3%AAng&sxsrf=APq-WBuCWOfqgbDsG2Sd-MXfjn5FB-uqwQ:1644236871965&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiT77fVy-31AhWUVPUHHcbrAEsQ_AUoAXoECAEQAw&biw=1366&bih=657&dpr=1#imgrc=-zgcabiycmgsiM' //link mess của các bạn. VD: https://m.me/nam.nodemy
